1c9d7670a3dcec8e406895365e61e8ddf7e1fe6b
Saswat Bhattacharya <sas.saswat@gmail.com>
2014-11-16 00:25:19 -0500

    Better hands Soccer!

---
+++ VrTest4/Assets/BallKick.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var speed = 1000.0;
+var bullet:Rigidbody;
+var ball:Transform;
+function Start () {
+}
+
+function Update () {
+if(Input.GetKeyDown("g")) {
+	rigidbody.AddForce(new Vector3(1,1,0) * speed, ForceMode.Impulse);
+}
+	/*if(Input.GetKeyDown("space")) {
+		var clone : Rigidbody;
+		clone = Instantiate(bullet, ball.transform.position,ball.transform.rotation);
+		clone.AddForce(new Vector3(0,1,1) * speed, ForceMode.Impulse); //Add impulse force in correct direction.
+	}*/
+}
+
+	function FixedUpdate () {
+		
+	}
---
+++ VrTest4/Assets/TriggerNew.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var bullet:Rigidbody;
+var ball:Transform;
+function Start () {
+
+}
+
+function Update () {
+if(Input.GetKeyDown("k")) {
+	Instantiate(bullet, ball.transform.position,ball.transform.rotation);
+}
+}
